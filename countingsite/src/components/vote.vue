7b78111937fc5170b84775eca60af041e7bdf81a
numselli <58607232+numselli@users.noreply.github.com>
2022-04-18 16:15:28 -0700

    Update vote.vue

--- countingsite/src/components/vote.vue
+++ countingsite/src/components/vote.vue
@@ -1,16 +1,16 @@
-<template></template>
+<template>
+</template>
 
 <script>
 export default {
   name: 'support',
     created() {
-        window.location.href = 'https://discord.gg/cz5ECsdPBT';
+      const votingSites = [
+        "https://top.gg/bot/726560538145849374/vote",
+        "https://discordbotlist.com/bots/counting-1881/upvote",
+        "https://bots.discordlabs.org/bot/726560538145849374/vote",
+      ]
+      window.location.href = votingSites[Math.floor((Math.random() * votingSites.length))];
     }
 }
 </script>
-<!-- const votingSites = [
-                "https://top.gg/bot/726560538145849374/vote",
-                "https://discordbotlist.com/bots/counting-1881/upvote",
-                "https://bots.discordlabs.org/bot/726560538145849374/vote",
-            ]
-            window.location.href = votingSites[Math.floor((Math.random() * votingSites.length))]; -->
