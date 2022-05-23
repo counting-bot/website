de518770df683745b792b453ac8bf63cdd6d0b2a
numselli <58607232+numselli@users.noreply.github.com>
2022-05-23 04:26:37 -0700

    add meta tags

--- src/components/vote.vue
+++ src/components/vote.vue
@@ -14,6 +14,72 @@ export default {
       //   "https://bots.discordlabs.org/bot/726560538145849374/vote",
       // ]
       // window.location.href = votingSites[Math.floor((Math.random() * votingSites.length))];
-    }
+    },
+     setup() {
+    const siteData = reactive({
+      title: `Counting`,
+      description: `The most customizable counting bot. Compete with other Discord servers to get the highest count.`,
+      url: "https://counting.numselli.xyz",
+      image: "https://counting.numselli.xyz/assets/img/CountingIconRound.png"
+    })
+
+    useHead({
+      title: computed(() => siteData.title),
+      meta: [
+        {
+          name: `title`,
+          content: computed(() => siteData.title),
+        },
+        {
+          name: `og:title`, 
+          content: computed(() => siteData.title),
+        },
+        {
+          name: "twitter:title",
+          content: computed(() => siteData.title),
+        },
+        {
+          name: `description`,
+          content: computed(() => siteData.description),
+        },
+        {
+          name: `og:description`,
+          content: computed(() => siteData.description),
+        },
+        {
+          name: "twitter:description",
+          content: computed(() => siteData.description),
+        },
+        {
+          name: "og:url",
+          content: computed(() => siteData.url)
+        },
+        {
+          name: "twitter:url",
+          content: computed(() => siteData.url)
+        },
+        {
+          name: "og:image",
+          content: computed(() => siteData.image)
+        },
+        {
+          name: "twitter:imag",
+          content: computed(() => siteData.image)
+        },
+        {
+          name: "og:type",
+          content: computed(() => "website")
+        },
+        {
+          name: `keywords`,
+          content: computed(() => "discord counting bot, counting bot, counting game, counting, counting discord"),
+        },
+        {
+          name: `theme-color`,
+          content: computed(() => "#a329d1"),
+        }
+      ]
+    })
+  }
 }
 </script>
