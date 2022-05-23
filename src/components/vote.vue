d1e2c12576543439815eca7c92ed3d9fe57e678d
numselli <58607232+numselli@users.noreply.github.com>
2022-05-23 04:24:05 -0700

    Update vote.vue

--- src/components/vote.vue
+++ src/components/vote.vue
@@ -8,12 +8,12 @@ export default {
       const userStats = await fetch(`https://api.numselli.xyz/discordOauth/user`, {credentials: "include"})
       if (userStats.status === 401) return window.location.href = `https://api.numselli.xyz/discordOauth/login?redirect_to=vote`; 
 
-      const votingSites = [
-        "https://top.gg/bot/726560538145849374/vote",
-        "https://discordbotlist.com/bots/counting-1881/upvote",
-        "https://bots.discordlabs.org/bot/726560538145849374/vote",
-      ]
-      window.location.href = votingSites[Math.floor((Math.random() * votingSites.length))];
+      // const votingSites = [
+      //   "https://top.gg/bot/726560538145849374/vote",
+      //   "https://discordbotlist.com/bots/counting-1881/upvote",
+      //   "https://bots.discordlabs.org/bot/726560538145849374/vote",
+      // ]
+      // window.location.href = votingSites[Math.floor((Math.random() * votingSites.length))];
     }
 }
 </script>
